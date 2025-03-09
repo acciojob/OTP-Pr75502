189a08186e4e7f3d9a8acfb93017304e20c72922
Pr75502 <pp1614582@gmail.com>
2025-03-09 22:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let inputBox = document.querySelectorAll(".code");
+
+inputBox.forEach((input, index) => {
+    input.addEventListener("input", () => {
+        if (input.value.length > 0 && index < inputBox.length - 1) {
+            inputBox[index + 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+            if (input.value === "" && index > 0) {
+                inputBox[index - 1].focus();
+            }
+        }
+        if (e.key === "Enter") {
+            if (input.value.length > 0 && index < inputBox.length - 1) {
+                inputBox[index + 1].focus();
+            }
+        }
+    });
+});
